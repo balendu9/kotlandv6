2b75e367cf581b38f1fdff905a617c0006c6681f
balendu9 <balendurawat9@gmail.com>
2025-08-06 17:42:14 +0530

    final

--- Quest.sol
+++ Quest.sol
@@ -4,13 +4,20 @@ pragma solidity ^0.8.20;
 import "./RegionNFT.sol";
 import "./Compute.sol";
 import "./Users.sol";
+
 contract QuestContract {
     address admin;
     Users public userContract;
     RegionNFT public tilecontract;
     Compute public computecontract;
     IERC20 public token;
-    constructor(address _userContract, address _tilecontractaddress, address _computecontract, address _token) {
+
+    constructor(
+        address _userContract,
+        address _tilecontractaddress,
+        address _computecontract,
+        address _token
+    ) {
         admin = msg.sender;
         userContract = Users(_userContract);
         tilecontract = RegionNFT(_tilecontractaddress);
@@ -18,82 +25,170 @@ contract QuestContract {
         token = IERC20(_token);
     }
 
-    function fetchTileInfo(uint256 regionId, uint32 tileId)
-    internal view returns (
-        uint32 id,
-        bool isBeingUsed,
-        bool isCrop,
-        uint8 cropTypeId,
-        uint8 factoryTypeId,
-        uint8 fertility,
-        uint8 waterLevel,
-        uint8 growthStage
-    ) {
+    function fetchTileInfo(
+        uint256 regionId,
+        uint32 tileId
+    )
+        internal
+        view
+        returns (
+            uint32 id,
+            bool isBeingUsed,
+            bool isCrop,
+            uint8 cropTypeId,
+            uint8 factoryTypeId,
+            uint8 fertility,
+            uint8 waterLevel,
+            uint8 growthStage
+        )
+    {
         return tilecontract.getTileData(regionId, tileId);
     }
-    
-    function fetchRegionInfo(uint256 regionId) internal view returns (
-        uint8 pollution,
-        uint8 fertility,
-        uint8 waterlevel,
-        uint8 eco,
-        uint256 lastupdate
-    ) {
+
+    function fetchRegionInfo(
+        uint256 regionId
+    )
+        internal
+        view
+        returns (
+            uint8 pollution,
+            uint8 fertility,
+            uint8 waterlevel,
+            uint8 eco,
+            uint256 lastupdate
+        )
+    {
         return tilecontract.getRegionMeta(regionId);
     }
 
     uint256 public cropPrice = 50000 * 10 ** 18;
-    function plantCrop(uint256 regionId, uint8 tileId, uint8 cropTypeId) external {
+
+    function plantCrop(
+        uint256 regionId,
+        uint8 tileId,
+        uint8 cropTypeId
+    ) external {
         (, bool isBeingUsed, , , , , , ) = fetchTileInfo(regionId, tileId);
-        require(!isBeingUsed && cropTypeId <=5, "INVALID_ACTION");
-        require(token.transferFrom(msg.sender, admin, cropPrice), "TOKEN_TRANSFER_FAILED");
-        tilecontract.setCropOrFactory(true, tileId, cropTypeId, msg.sender, regionId);
+        require(!isBeingUsed && cropTypeId <= 5, "INVALID_ACTION");
+        require(
+            token.transferFrom(msg.sender, admin, cropPrice),
+            "TOKEN_TRANSFER_FAILED"
+        );
+        tilecontract.setCropOrFactory(
+            true,
+            tileId,
+            cropTypeId,
+            msg.sender,
+            regionId
+        );
         recordAction(msg.sender, 1);
         userContract.updateUserExp(msg.sender, 7);
     }
 
     uint256 public factoryPrice = 500000 * 10 ** 18;
-    function buildFactory(uint256 regionId, uint8 tileId, uint8 _factoryTypeId) external {
+
+    function buildFactory(
+        uint256 regionId,
+        uint8 tileId,
+        uint8 _factoryTypeId
+    ) external {
         (, bool isBeingUsed, , , , , , ) = fetchTileInfo(regionId, tileId);
         require(!isBeingUsed && _factoryTypeId <= 5, "INVALID");
-        require(token.transferFrom(msg.sender, admin, factoryPrice), "TOKEN_TRANSFER_FAILED");
-        tilecontract.setCropOrFactory(false, tileId, _factoryTypeId, msg.sender, regionId);      
+        require(
+            token.transferFrom(msg.sender, admin, factoryPrice),
+            "TOKEN_TRANSFER_FAILED"
+        );
+        tilecontract.setCropOrFactory(
+            false,
+            tileId,
+            _factoryTypeId,
+            msg.sender,
+            regionId
+        );
         recordAction(msg.sender, 2);
         userContract.updateUserExp(msg.sender, 13);
     }
 
     mapping(uint256 => mapping(uint32 => uint256)) public lastWateredTime;
+
     function waterCrop(uint256 regionId, uint32 tileId) external {
-        (, bool isBeingUsed, bool isCrop , uint8 cropTypeId, , uint8 fertility, uint8 waterLevel, ) = fetchTileInfo(regionId, tileId);
-        require(isBeingUsed && isCrop && block.timestamp > lastWateredTime[regionId][tileId] + 1 days, "ONCE_IN_24_HOURS");
+        (
+            ,
+            bool isBeingUsed,
+            bool isCrop,
+            uint8 cropTypeId,
+            ,
+            uint8 fertility,
+            uint8 waterLevel,
+
+        ) = fetchTileInfo(regionId, tileId);
+        require(
+            isBeingUsed &&
+                isCrop &&
+                block.timestamp > lastWateredTime[regionId][tileId] + 1 days,
+            "ONCE_IN_24_HOURS"
+        );
         lastWateredTime[regionId][tileId] = block.timestamp;
-        
+
         (uint8 pollution, , , uint8 eco, ) = fetchRegionInfo(regionId);
 
         uint8 growth = computecontract.plantGrowthCalculator(
-            cropTypeId, fertility, waterLevel, eco,  pollution, true, msg.sender
+            cropTypeId,
+            fertility,
+            waterLevel,
+            eco,
+            pollution,
+            true,
+            msg.sender
         );
         // uint8 _cropType, uint8 _fertility, uint8 _waterlevel, uint8 _ecoscore, uint8 _pollutionlevel, bool worf, address _user
         tilecontract.updateWFG(tileId, true, growth, regionId, msg.sender);
 
         userContract.updateUserExp(msg.sender, 2);
     }
-// updateWFG(uint32 tileId, bool worf, uint8 growth, uint256 regionId, address _user)
+
+    // updateWFG(uint32 tileId, bool worf, uint8 growth, uint256 regionId, address _user)
 
     function fertilizeCrop(uint256 regionId, uint32 tileId) external {
-        (, bool isBeingUsed, bool isCrop , uint8 cropTypeId, , uint8 fertility, uint8 waterLevel, ) = fetchTileInfo(regionId, tileId);
+        (
+            ,
+            bool isBeingUsed,
+            bool isCrop,
+            uint8 cropTypeId,
+            ,
+            uint8 fertility,
+            uint8 waterLevel,
+
+        ) = fetchTileInfo(regionId, tileId);
         require(isBeingUsed && isCrop && fertility <= 100, "INVALID");
-        (uint8 pollution, uint8 fertilityR, , uint8 eco, ) = fetchRegionInfo(regionId);
+        (uint8 pollution, uint8 fertilityR, , uint8 eco, ) = fetchRegionInfo(
+            regionId
+        );
 
         uint8 growth = computecontract.plantGrowthCalculator(
-            cropTypeId, fertilityR, waterLevel, eco,  pollution, true, msg.sender
+            cropTypeId,
+            fertilityR,
+            waterLevel,
+            eco,
+            pollution,
+            true,
+            msg.sender
         );
         tilecontract.updateWFG(tileId, false, growth, regionId, msg.sender);
         userContract.updateUserExp(msg.sender, 4);
     }
 
     function harvestCrop(uint256 regionId, uint32 tileId) external {
-        (, bool isBeingUsed, bool isCrop , uint8 cropTypeId, , , , uint8 growthStage) = fetchTileInfo(regionId, tileId);
+        (
+            ,
+            bool isBeingUsed,
+            bool isCrop,
+            uint8 cropTypeId,
+            ,
+            ,
+            ,
+            uint8 growthStage
+        ) = fetchTileInfo(regionId, tileId);
         require(isBeingUsed && isCrop && growthStage == 100, "INVALID");
         tilecontract.updateAfterHarvest(tileId, regionId, msg.sender);
         computecontract.getHarvestedResourceAndAmount(cropTypeId, msg.sender);
@@ -101,15 +196,30 @@ contract QuestContract {
         userContract.updateUserExp(msg.sender, 15);
     }
 
-    function produceFromFactory(uint256 regionId, uint32 tileId, uint8 _factoryTypeId) external {
-        (, bool isBeingUsed, bool isCrop , , uint8 factoryTypeId, , , ) = fetchTileInfo(regionId, tileId);
-        require(isBeingUsed && !isCrop && _factoryTypeId == factoryTypeId, "INVALID");
+    function produceFromFactory(
+        uint256 regionId,
+        uint32 tileId,
+        uint8 _factoryTypeId
+    ) external {
+        (
+            ,
+            bool isBeingUsed,
+            bool isCrop,
+            ,
+            uint8 factoryTypeId,
+            ,
+            ,
+
+        ) = fetchTileInfo(regionId, tileId);
+        require(
+            isBeingUsed && !isCrop && _factoryTypeId == factoryTypeId,
+            "INVALID"
+        );
         computecontract._produceFromFactory(msg.sender, factoryTypeId);
         recordAction(msg.sender, 4);
 
         userContract.updateUserExp(msg.sender, 8);
     }
-
 
     // quests
     function getCurrentDay() public view returns (uint256) {
@@ -130,36 +240,33 @@ contract QuestContract {
         uint8 cropsHarvested;
         uint8 factoryProduced;
     }
-     
+
     struct Quest {
-        string name; 
+        string name;
+        string description;
         uint256 id;
         bool daily;
-
         uint256 currentParticipants;
         uint256 maxParticipants;
         address[] participantList;
-
         uint8 actionToDo;
         uint256 amountToComplete;
         uint256 reward;
         uint8 assetType;
-        
         uint256 endDay;
-
         mapping(address => bool) participants;
         mapping(address => bool) claimed;
     }
 
     uint256 public nextQuestId = 1;
     mapping(uint256 => Quest) public quests;
-    
+
     mapping(address => DailyProgress) public progress;
     mapping(address => LongProgress) public longprogress;
 
-
     function createQuest(
         string memory _name,
+        string memory _description,
         bool _daily,
         uint8 _actionToDo,
         uint256 _amountToComplete,
@@ -172,6 +279,7 @@ contract QuestContract {
         Quest storage q = quests[questId];
 
         q.name = _name;
+        q.description = _description;
         q.id = questId;
         q.daily = _daily;
         q.actionToDo = _actionToDo;
@@ -179,8 +287,8 @@ contract QuestContract {
         q.reward = _reward;
         q.assetType = _assetType;
         q.maxParticipants = _maxParticipants;
-        
-        q.endDay =  getCurrentDay() + _endDay;
+
+        q.endDay = getCurrentDay() + _endDay;
         nextQuestId++;
     }
 
@@ -191,9 +299,9 @@ contract QuestContract {
         require(today <= q.endDay, "Quest has expired");
 
         require(!q.participants[msg.sender], "ALREADY_JOINED");
-        
+
         require(q.currentParticipants < q.maxParticipants, "QUEST_FULL");
-        
+
         q.participants[msg.sender] = true;
         q.participantList.push(msg.sender);
         q.currentParticipants++;
@@ -210,8 +318,8 @@ contract QuestContract {
             p.factoriesBuilt = 0;
             p.cropsHarvested = 0;
             p.factoryProduced = 0;
-        } 
-        
+        }
+
         // 1 = plant, 2 = build, 3 = harvest, 4 = produce
         if (action == 1) {
             p.cropsPlanted++;
@@ -225,10 +333,9 @@ contract QuestContract {
         } else if (action == 4) {
             p.factoryProduced++;
             l.factoryProduced++;
-        } 
+        }
         p.currDay = currentDay;
     }
-
 
     function claimReward(uint256 questId) external {
         Quest storage q = quests[questId];
@@ -241,13 +348,13 @@ contract QuestContract {
 
         DailyProgress storage dp = progress[msg.sender];
         LongProgress storage lp = longprogress[msg.sender];
-        
+
         uint256 userProgress;
         uint256 reward;
 
         uint8 assettype;
         if (q.daily) {
-            if(q.actionToDo == 1){
+            if (q.actionToDo == 1) {
                 userProgress = dp.cropsPlanted;
             } else if (q.actionToDo == 2) {
                 userProgress = dp.factoriesBuilt;
@@ -263,15 +370,13 @@ contract QuestContract {
             reward = q.reward;
             assettype = q.assetType;
 
-
             dp.currDay = today;
             dp.cropsPlanted = 0;
             dp.factoriesBuilt = 0;
             dp.cropsHarvested = 0;
             dp.factoryProduced = 0;
-
         } else {
-            if(q.actionToDo == 1){
+            if (q.actionToDo == 1) {
                 userProgress = lp.cropsPlanted;
             } else if (q.actionToDo == 2) {
                 userProgress = lp.factoriesBuilt;
@@ -291,19 +396,60 @@ contract QuestContract {
             lp.factoriesBuilt = 0;
             lp.cropsHarvested = 0;
             lp.factoryProduced = 0;
-
-
         }
 
         if (assettype == 0) {
-            require(token.transfer(msg.sender, reward), "TOKEN_TRANSFER_FAILED");
+            require(
+                token.transfer(msg.sender, reward),
+                "TOKEN_TRANSFER_FAILED"
+            );
         } else {
-            userContract.updateInventory(msg.sender, assettype, uint8(reward), true);
-        }  
-
+            userContract.updateInventory(
+                msg.sender,
+                assettype,
+                uint8(reward),
+                true
+            );
+        }
 
         userContract.updateUserExp(msg.sender, 31);
-        
     }
 
-} 
+    function getQuest(
+        uint256 questId
+    )
+        external
+        view
+        returns (
+            string memory name,
+            string memory description,
+            uint256 id,
+            bool daily,
+            uint256 currentParticipants,
+            uint256 maxParticipants,
+            address[] memory participantList,
+            uint8 actionToDo,
+            uint256 amountToComplete,
+            uint256 reward,
+            uint8 assetType,
+            uint256 endDay
+        )
+    {
+        Quest storage q = quests[questId];
+
+        return (
+            q.name,
+            q.description,
+            q.id,
+            q.daily,
+            q.currentParticipants,
+            q.maxParticipants,
+            q.participantList,
+            q.actionToDo,
+            q.amountToComplete,
+            q.reward,
+            q.assetType,
+            q.endDay
+        );
+    }
+}
